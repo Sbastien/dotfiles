0244ac66296423ad1adc3b4e69f8e17534201e54
Sébastien Loyer <loyer.sebastien@gmail.com>
2025-01-10 17:28:56 +0100

    🔧 Update gitconfig & zsh aliases

--- dot_config/zshrc/aliases.zsh
+++ dot_config/zshrc/aliases.zsh
@@ -3,3 +3,6 @@ alias reload="source ~/.zshrc"
 
 # edit zshrc
 alias zs="code $HOME/.local/share/chezmoi"
+
+# Navigate to iCloud directory
+alias icloud="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/"
