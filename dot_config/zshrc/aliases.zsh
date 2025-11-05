2fba4a456ea950a8fd61c741efe18fad1cf26211
Sébastien Loyer <sl@teeptrak.com>
2025-11-05 09:06:35 +0100

    update: add nvm & projects alias

--- dot_config/zshrc/aliases.zsh
+++ dot_config/zshrc/aliases.zsh
@@ -6,3 +6,6 @@ alias zs="code $HOME/.local/share/chezmoi"
 
 # Navigate to iCloud directory
 alias icloud="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/"
+
+# Navigate to projects directory
+alias projects="cd ~/projects/"
--- dot_config/zshrc/init.zsh
+++ dot_config/zshrc/init.zsh
@@ -9,3 +9,6 @@ source $ZSH/oh-my-zsh.sh
 
 # Starship prompt configuration
 eval "$(starship init zsh)"
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
